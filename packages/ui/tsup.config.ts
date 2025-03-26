32b6b6bfde36de051e726c72a581b366c4eada81
Jean-Philippe Sirois <jp@veksen.net>
2025-03-25 21:57:20 -0400

    wip

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig((options) => ({
-  entryPoints: ["src/button.tsx"],
+  entryPoints: ["src/button.tsx", "src/logo.tsx"],
   format: ["cjs", "esm"],
   dts: true,
   sourcemap: true,
