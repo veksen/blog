6829b12d9baab82b105d6570ad6ee4f1c2d71277
Jean-Philippe Sirois <jp@veksen.net>
2025-04-14 17:06:25 +0400

    fix: types for deployment

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,9 +1,11 @@
+import React from "react";
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children, ...other }: ButtonProps): JSX.Element {
+export function Button({ children, ...other }: ButtonProps): React.JSX.Element {
   return (
     <button type="button" {...other}>
       {children}
