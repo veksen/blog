dae05426d29472ff43b9433fb4360dc73a8bc3c8
Jean-Philippe Sirois <jp@veksen.net>
2025-07-28 09:23:37 +0400

    build: use Astro's static output

--- apps/blog/astro.config.mjs
+++ apps/blog/astro.config.mjs
@@ -9,6 +9,6 @@ import react from '@astrojs/react';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap(), react()],
-	output: "server",
+	output: "static",
 	adapter: node({ mode: "standalone" })
 });
