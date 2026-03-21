31310d08b35dd8275d9177276751a3c2e82012de
Jean-Philippe Sirois <jp@veksen.net>
2026-03-21 15:58:35 +0100

    feat: update site title/description

--- apps/blog/src/consts.ts
+++ apps/blog/src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Jean-Philippe Sirois - web dev & designer";
+export const SITE_DESCRIPTION = "specialized in UI and front-end development, experienced with the whole stack";
